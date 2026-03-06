0f94ddbffbe11054bcb2f370419a81b7b1a1e499
fahri430 <fahrizalazs01@gmail.com>
2026-03-06 22:52:02 +0700

    Update author information and maintain insertion sort

--- Praktikum6/Insertion2.py
+++ Praktikum6/Insertion2.py
@@ -1,6 +1,6 @@
 #==============================================
-#Nama   : Muhammad Rafly
-#NIM    : J0403251002
+#Nama   : Fahrizal Azis
+#NIM    : J0403251151
 #Kelas  : A
 #==============================================
 
@@ -18,4 +18,5 @@ def insertionSort(data):
 
 data = [54,26,93,17,77,31,44,55,20]
 insertionSort(data)
+
 print(data)
