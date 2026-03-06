3a87069973570848051993926ed8c974efb7dde3
fahri430 <fahrizalazs01@gmail.com>
2026-03-06 22:58:21 +0700

    Update selection1.py

--- Praktikum6/selection1.py
+++ Praktikum6/selection1.py
@@ -1,6 +1,6 @@
 #==============================================
-#Nama   : Muhammad Rafly
-#NIM    : J0403251002
+#Nama   : Fahrizal Azis
+#NIM    : J0403251151
 #Kelas  : A
 #==============================================
 
@@ -21,4 +21,5 @@ def selectionSort(data):
 
 data = [54,26,93,17,77,31,44,55,20]
 selectionSort(data)
+
 print(data)
